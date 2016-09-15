05aa3265b9941575140e60be87d2b7235c01ed0e
taphap <hzhongh@gmail.com>
2016-09-15 12:46:49 +0800

    数组栈

--- stack/arrayStack.c
+++ stack/arrayStack.c
@@ -1,30 +1,31 @@
 #include "arrayStack.h"
-#include <string.h>
 
 void initStack(STACK_S* stack)
 {
-	memset(stack, 0, sizeof(STACK_S));
+    memset(stack, 0, sizeof(STACK_S));
 }
 
 bool isEmpty(STACK_S* stack)
 {
-	return stack->num == 0;
+    return stack->num == 0;
 }
 
 size_type size(STACK_S* stack)
 {
-	return stack->num;
+    return stack->num;
 }
 
+/* 调用者需保证栈未满 */
 void push(STACK_S* stack, item_type item)
 {
-	stack->items[stack->num] = item;
-	stack->num++;
-	return;
+    stack->items[stack->num] = item;
+    stack->num++;
+    return;
 }
 
+/* 调用者需保证栈非空 */
 item_type pop(STACK_S* stack)
 {
-	stack->num--;
-	return stack->items[stack->num];
+    stack->num--;
+    return stack->items[stack->num];
 }
--- stack/arrayStack.h
+++ stack/arrayStack.h
@@ -1,5 +1,4 @@
-#ifndef STACK_H
-#define STACK_H
+#pragma once
 
 /* 栈大小 */
 #define STACK_SIZE (10)
@@ -10,12 +9,10 @@ typedef int item_type;
 /* 栈大小类型 */
 typedef unsigned int size_type;
 
-typedef int bool;
-
 typedef struct tagSTACK_S
 {
-	size_type num;
-	item_type  items[STACK_SIZE];
+    size_type num;
+    item_type  items[STACK_SIZE];
 } STACK_S;
 
 /* 外部接口 */
@@ -24,5 +21,3 @@ bool isEmpty(STACK_S* stack);
 size_type size(STACK_S* stack);
 void push(STACK_S* stack, item_type item);
 item_type pop(STACK_S* stack);
-
-#endif
