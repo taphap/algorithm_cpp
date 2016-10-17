86a6aa0a3df7fa61a2526be643dd7414ea820003
taphap <hzhongh@gmail.com>
2016-10-17 23:59:11 +0800

    增加查找类数据结构说明

--- logTool/liblogger/src/testapp/logtest.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LOG_TEST_H__
-#define __LOG_TEST_H__
-int TestFuncFatal();
-int TestFuncWarn();
-int TestFuncDebug();
-int TestNoFilename();
-int TestFuncMin();
-int TestFuncNoLogs();
-#endif // __LOG_TEST_H__
